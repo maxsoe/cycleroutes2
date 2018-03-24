f78e6abafd71a05b2c9ccb3d4222038fe1944adb
Max Soe <maxsoe@gmail.com>
2018-03-24 00:56:06 +0000

    passing data between components

--- all-routes/all-routes.component.js
+++ all-routes/all-routes.component.js
@@ -12,5 +12,13 @@ angular
 function allRoutesController(getRoutesListSvc) {
   getRoutesListSvc.getPosts().then(response => {
     this.routes = response.data;
+    // this.gpxFile = this.routes[0].name;
+    this.gpxFile = "spawn";
+    console.log(this.gpxFile);
+    // angular.forEach(this.routes, function(value, key) {
+    //   // console.log(value.gpx);
+    //   // processThisGpx(value.gpx);
+    // });
   });
+
 };
--- gpx-on-map/gpx-on-map.component.js
+++ gpx-on-map/gpx-on-map.component.js
@@ -1,29 +1,50 @@
 angular
-  .module('gpxOnMap', [
-  ])
+  .module('gpxOnMap', [])
   .component('gpxOnMap', {
     // template: 'test template'
     templateUrl: 'gpx-on-map/gpx-on-map.template.html',
     css: 'gpx-on-map/gpx-on-map.css',
-    controller: gpxOnMapController,
-    controllerAs: 'gpxOnMapCtrl'
+    controller: [
+      gpxOnMapController
+    ],
+    controllerAs: 'gpxOnMapCtrl',
+    bindings: {
+      hero: '='
+    }
   });
 
+// function processThisGpx(gpxFile) {
+//   // console.log("The GPX file is " +gpxFile);
+// }
+
 function gpxOnMapController() {
-  var map = L.map('map');
+  // var gpxOnMapCtrl = this;
+  // console.log("The GPX file found in the controller is");
+  // gpxOnMapCtrl.$onInit = function(){
+  //   console.log(gpxOnMapCtrl.gpxFile);
+  // }
+  var map = [];
+  map[0] = L.map('map');
   // document.getElementById('weathermap').innerHTML = "<div id='map' style='width: 100%; height: 100%;'></div>";
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map[0]);
   var gpx = 'data/gpx/test.gpx'; // URL to your GPX file or the GPX itself
   new L.GPX(gpx, {
     async: true,
     marker_options: {
-    startIconUrl: 'gpx-on-map/marker/pin-icon-start.png',
-    endIconUrl: 'gpx-on-map/marker/pin-icon-end.png',
-    shadowUrl: 'gpx-on-map/marker/pin-shadow.png'
-  }
+      startIconUrl: 'gpx-on-map/marker/pin-icon-start.png',
+      endIconUrl: 'gpx-on-map/marker/pin-icon-end.png',
+      shadowUrl: 'gpx-on-map/marker/pin-shadow.png'
+    }
   }).on('loaded', function(e) {
-    map.fitBounds(e.target.getBounds());
-  }).addTo(map);
+    map[0].fitBounds(e.target.getBounds());
+  }).addTo(map[0]);
+
+  map[0].touchZoom.disable();
+  map[0].doubleClickZoom.disable();
+  map[0].scrollWheelZoom.disable();
+  map[0].boxZoom.disable();
+  map[0].keyboard.disable();
+  map[0].dragging.disable();
 
   // var map2 = L.map('map2');
   // L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map2);
