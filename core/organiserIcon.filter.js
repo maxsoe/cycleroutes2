a6e6d85307f4ec05a30ed736c87e018e0bab2baf
Max Soe <maxsoe@gmail.com>
2018-03-27 17:16:06 +0100

    simplify organiseIcon filter by omitting switch

--- core/organiserIcon.filter.js
+++ core/organiserIcon.filter.js
@@ -3,33 +3,18 @@ angular
   .module('organiserIconFtl', [])
   .filter('organiserIcon', function() {
     return function(input) {
-      switch (input) {
-        case 'altura':
-          return ("altura")
-          break;
-        case 'brewin':
-          return ("brewin")
-          break;
-        case 'ctc':
-          return ("ctc")
-          break;
-        case 'noexcuses':
-          return ("noexcuses")
-          break;
-        case 'roo':
-          return ("roo")
-          break;
-        case 'sigma':
-          return ("sigma")
-          break;
-        case 'ukce':
-          return ("ukce")
-          break;
-        case 'wiggle':
-          return ("wiggle")
-          break;
-        default:
-          return ("friends")
+      if (input == "altura" ||
+        input == "brewin" ||
+        input == "ctc" ||
+        input == "noexcuses" ||
+        input == "roo" ||
+        input == "sigma" ||
+        input == "ukce" ||
+        input == "wiggle") {
+        return input;
+      } else {
+        return "friends";
       }
     };
+
   });
